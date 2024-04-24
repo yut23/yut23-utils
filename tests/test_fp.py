yut23/yut23-utils

commit ce8af86532a6a0caabd957bfee24c8b9254ecd06

Remove unnecessary abs() in ulp_diff()

src/yut23_utils/fp.py
@@ -31,10 +31,11 @@ def ulp_diff(a: float, b: float, /, *, include_sign: bool = False) -> int:
         if include_sign:
             ulps *= -1
         return ulps
+    # b is now greater than a
     if math.copysign(1.0, a) != math.copysign(1.0, b):
         # different signs: split the interval at zero
         return ulp_diff(a, -0.0) + ulp_diff(0.0, b)
-    return abs(float_to_int(a) - float_to_int(b))
+    return float_to_int(b) - float_to_int(a)
 
 
 def compare_ulp(a: float, b: float, /, ulps: int) -> bool:

commit ec2154c96558916aaf24beaf4a6d3dfd9475d47e

Fix error in ulp_diff introduced in ce8af86
Also add more tests

src/yut23_utils/fp.py
@@ -24,6 +24,8 @@ def ulp_diff(a: float, b: float, /, *, include_sign: bool = False) -> int:
         msg = "only finite values can be compared"
         raise ValueError(msg)
     if a == b:
+        # this case also covers 0.0 vs -0.0, which compare equal but have
+        # different representations
         return 0
     if a > b:
         # pylint: disable-next=arguments-out-of-order
@@ -31,11 +33,13 @@ def ulp_diff(a: float, b: float, /, *, include_sign: bool = False) -> int:
         if include_sign:
             ulps *= -1
         return ulps
-    # b is now greater than a
+    # b is now greater than a in value
     if math.copysign(1.0, a) != math.copysign(1.0, b):
         # different signs: split the interval at zero
         return ulp_diff(a, -0.0) + ulp_diff(0.0, b)
-    return float_to_int(b) - float_to_int(a)
+    # subtract the smaller in magnitude from the larger
+    smaller, larger = sorted((a, b), key=abs)
+    return float_to_int(larger) - float_to_int(smaller)
 
 
 def compare_ulp(a: float, b: float, /, ulps: int) -> bool:

tests/test_fp.py
@@ -54,12 +54,18 @@ def float_pairs(draw: st.DrawFn, max_ulps: int) -> tuple[float, float, int]:
 @given(float_pairs(max_ulps=100))
 @example((0.0, -5e-324, -1))
 @example((5e-324, 0.0, -1))
-@example((5e-324, -0.0, -1))
 @example((-5e-324, 5e-324, 2))
+@example((1, 2, 1 << 52))
+@example((2, 4, 1 << 52))
+@example((0, 1, 0x3FF << 52))
 def test_ulp_diff(args: tuple[float, float, int]) -> None:
     a, b, ulps = args
     assert ulp_diff(a, b) == abs(ulps)
+    assert ulp_diff(b, a) == abs(ulps)  # pylint: disable=arguments-out-of-order
     assert ulp_diff(a, b, include_sign=True) == ulps
+    assert ulp_diff(b, a, include_sign=True) == -ulps
+    assert ulp_diff(-b, -a, include_sign=True) == ulps
+    assert ulp_diff(-a, -b, include_sign=True) == -ulps
 
 
 @pytest.mark.parametrize(
@@ -95,7 +101,7 @@ def test_compare_ulp(args: tuple[float, float, int]) -> None:
 
 def test_compare_ulp_errors() -> None:
     with pytest.raises(ValueError, match="non-negative"):
-        compare_ulp(1.0, 1.0, -1)
+        compare_ulp(1.0, 1.0, ulps=-1)
 
 
 @given(st.floats())