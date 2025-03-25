9769d99f2b1d420f52161fed0a0b16ef9241abf1
yut23 <yut23@gvljohnsons.com>
2025-03-24 21:56:30 -0400

    Add import_from_path() from the importlib docs
    This is just handy to have available.

--- src/yut23_utils/__init__.py
+++ src/yut23_utils/__init__.py
@@ -3,3 +3,4 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from yut23_utils.fp import FloatInspector, compare_ulp, ulp_diff
+from yut23_utils.imports import import_from_path
---
+++ src/yut23_utils/imports.py
@@ -0,0 +1,16 @@
+import importlib.util
+import os
+import sys
+from types import ModuleType
+
+
+def import_from_path(module_name: str, file_path: os.PathLike[str]) -> ModuleType:
+    """https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly"""
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    if spec is None or spec.loader is None:
+        msg = "importlib.util.spec_from_file_location() returned an invalid module spec"
+        raise ImportError(msg, name=module_name)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
