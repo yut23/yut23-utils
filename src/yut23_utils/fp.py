d5c6919df4e670977b76e577189f12c255e862fa
yut23 <yut23@gvljohnsons.com>
2025-12-03 12:42:56 -0800

    Allow working with different floating-point precisions
    All functions and FloatInspector now take a `prec` keyword argument,
    defaulting to double precision.

--- src/yut23_utils/fp.py
+++ src/yut23_utils/fp.py
@@ -3,23 +3,120 @@
 # SPDX-License-Identifier: BSD-3-Clause
 """Tools for working with floating-point numbers."""
 
+from __future__ import annotations
+
+import enum
 import math
 import struct
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from functools import cached_property
 from typing import cast
 
 
-def float_to_int(x: float, /) -> int:
-    return cast(int, struct.unpack("<Q", struct.pack("<d", x))[0])
+class Precision(enum.Enum):
+    BINARY16 = 16
+    BINARY32 = 32
+    BINARY64 = 64
+    HALF = BINARY16
+    SINGLE = BINARY32
+    DOUBLE = BINARY64
+
+    @cached_property
+    def total_bits(self) -> int:
+        return self.value
+
+    @cached_property
+    def exp_bits(self) -> int:
+        return {
+            Precision.BINARY16: 5,
+            Precision.BINARY32: 8,
+            Precision.BINARY64: 11,
+        }[self]
+
+    @cached_property
+    def mant_bits(self) -> int:
+        return {
+            Precision.BINARY16: 10,
+            Precision.BINARY32: 23,
+            Precision.BINARY64: 52,
+        }[self]
+
+    @cached_property
+    def float_struct_fmt(self) -> str:
+        return {
+            Precision.BINARY16: "e",
+            Precision.BINARY32: "f",
+            Precision.BINARY64: "d",
+        }[self]
+
+    @cached_property
+    def uint_struct_fmt(self) -> str:
+        return {
+            Precision.BINARY16: "H",
+            Precision.BINARY32: "L",
+            Precision.BINARY64: "Q",
+        }[self]
+
+    @cached_property
+    def int_struct_fmt(self) -> str:
+        return self.uint_struct_fmt.lower()
+
+    @cached_property
+    def bias(self) -> int:
+        return (1 << (self.exp_bits - 1)) - 1
 
 
-def int_to_float(q: int, /) -> float:
-    return cast(float, struct.unpack("<d", struct.pack("<Q", q))[0])
+def float_to_uint(x: float, /, prec: Precision = Precision.DOUBLE) -> int:
+    return cast(
+        int,
+        struct.unpack(
+            "<" + prec.uint_struct_fmt, struct.pack("<" + prec.float_struct_fmt, x)
+        )[0],
+    )
 
 
-def ulp_diff(a: float, b: float, /, *, include_sign: bool = False) -> int:
-    """Return the number of representable FP64 values in the range [a, b)."""
+def float_to_sint(x: float, /, prec: Precision = Precision.DOUBLE) -> int:
+    return cast(
+        int,
+        struct.unpack(
+            "<" + prec.int_struct_fmt, struct.pack("<" + prec.float_struct_fmt, x)
+        )[0],
+    )
+
+
+float_to_int = float_to_uint
+
+
+def uint_to_float(q: int, /, prec: Precision = Precision.DOUBLE) -> float:
+    return cast(
+        float,
+        struct.unpack(
+            "<" + prec.float_struct_fmt, struct.pack("<" + prec.uint_struct_fmt, q)
+        )[0],
+    )
+
+
+def sint_to_float(q: int, /, prec: Precision = Precision.DOUBLE) -> float:
+    return cast(
+        float,
+        struct.unpack(
+            "<" + prec.float_struct_fmt, struct.pack("<" + prec.int_struct_fmt, q)
+        )[0],
+    )
+
+
+int_to_float = uint_to_float
+
+
+def ulp_diff(
+    a: float,
+    b: float,
+    /,
+    prec: Precision = Precision.DOUBLE,
+    *,
+    include_sign: bool = False,
+) -> int:
+    """Return the number of representable `prec` values in the range [a, b)."""
     if not math.isfinite(a) or not math.isfinite(b):
         msg = "only finite values can be compared"
         raise ValueError(msg)
@@ -29,61 +126,105 @@ def ulp_diff(a: float, b: float, /, *, include_sign: bool = False) -> int:
         return 0
     if a > b:
         # pylint: disable-next=arguments-out-of-order
-        ulps = ulp_diff(b, a)
+        ulps = ulp_diff(b, a, prec)
         if include_sign:
             ulps *= -1
         return ulps
     # b is now greater than a in value
     if math.copysign(1.0, a) != math.copysign(1.0, b):
         # different signs: split the interval at zero
-        return ulp_diff(a, -0.0) + ulp_diff(0.0, b)
+        return ulp_diff(a, -0.0, prec=prec) + ulp_diff(0.0, b, prec)
     # subtract the smaller in magnitude from the larger
     smaller, larger = sorted((a, b), key=abs)
-    return float_to_int(larger) - float_to_int(smaller)
+    return float_to_uint(larger, prec) - float_to_uint(smaller, prec)
 
 
-def compare_ulp(a: float, b: float, /, ulps: int) -> bool:
-    """Check if two numbers match to within a specified number of FP64 ULPs."""
+def compare_ulp(
+    a: float, b: float, /, ulps: int, prec: Precision = Precision.DOUBLE
+) -> bool:
+    """Check if two numbers match to within a specified number of `prec` ULPs."""
     if ulps < 0:
         msg = "ulps must be non-negative"
         raise ValueError(msg)
-    return ulp_diff(a, b, include_sign=False) <= ulps
+    return ulp_diff(a, b, prec, include_sign=False) <= ulps
 
 
 def _make_mask(bits: int) -> int:
     return (1 << bits) - 1
 
 
+def nextafter(x: float, y: float, /, prec: Precision = Precision.DOUBLE) -> float:
+    # based on glibc implementation
+    hx = float_to_uint(x, prec)
+    hy = float_to_uint(y, prec)
+    # mask out the sign bit
+    ix = hx & _make_mask(prec.total_bits - 1)
+    # check for NaN
+    if math.isnan(x) or math.isnan(y):
+        return x + y
+    if x == y:
+        return y
+    if ix == 0:
+        # return +/- minsubnormal
+        # 0x4010_0000_0000_0001
+        return uint_to_float(hy & (1 << (prec.total_bits - 1)) | 1, prec)
+    if x >= 0:  # x > 0
+        if x > y:  # x > y, x -= ulp
+            hx -= 1
+        else:  # x < y, x += ulp
+            hx += 1
+    else:  # x < 0, hx has the opposite sign  # noqa: PLR5501
+        if x < y:  # x < y, x -= ulp
+            hx -= 1
+        else:  # x > y, x += ulp
+            hx += 1
+    return uint_to_float(hx, prec)
+
+
 @dataclass(frozen=True)
 class FloatInspector:
     float_val: float
-    int_val: int = field(init=False)
+    int_val: int
+    prec: Precision = Precision.DOUBLE
 
-    EXP_BITS = 11
-    MANT_BITS = 52
-
-    def __post_init__(self):
-        object.__setattr__(self, "int_val", float_to_int(self.float_val))
+    def __init__(
+        self,
+        float_val: float | None = None,
+        *,
+        int_val: int | None = None,
+        prec: Precision = Precision.DOUBLE,
+    ):
+        object.__setattr__(self, "prec", prec)
+        if (float_val is None) == (int_val is None):
+            msg = "exactly one of float_val and int_val must be specified"
+            raise TypeError(msg)
+        if float_val is not None:
+            int_val = float_to_uint(float_val, self.prec)
+        assert int_val is not None  # noqa: S101
+        # cast back to a float to get the exact value for the given precision
+        float_val = uint_to_float(int_val, self.prec)
+        object.__setattr__(self, "float_val", float_val)
+        object.__setattr__(self, "int_val", int_val)
 
     @cached_property
     def raw_sign(self) -> int:
-        return (self.int_val >> (self.EXP_BITS + self.MANT_BITS)) & 1
+        return (self.int_val >> (self.prec.exp_bits + self.prec.mant_bits)) & 1
 
     @cached_property
     def raw_exponent(self) -> int:
-        return (self.int_val >> self.MANT_BITS) & _make_mask(self.EXP_BITS)
+        return (self.int_val >> self.prec.mant_bits) & _make_mask(self.prec.exp_bits)
 
     @cached_property
     def raw_mantissa(self) -> int:
-        return self.int_val & _make_mask(self.MANT_BITS)
+        return self.int_val & _make_mask(self.prec.mant_bits)
 
     def __str__(self) -> str:
         return (
             f"FloatData({float(self)} = "
             f"{self.raw_sign} * 2^{self.exponent} * {self.mantissa}; "
             f"s={self.raw_sign}, "
-            f"e={self.raw_exponent:011b}, "
-            f"m={self.raw_mantissa:052b})"
+            f"e={self.raw_exponent:0{self.prec.exp_bits}b}, "
+            f"m=(1){self.raw_mantissa:0{self.prec.mant_bits}b})"
         )
 
     def __repr__(self) -> str:
@@ -95,11 +236,11 @@ class FloatInspector:
 
     @cached_property
     def exponent(self) -> int:
-        return self.raw_exponent - 1023
+        return self.raw_exponent - ((1 << (self.prec.exp_bits - 1)) - 1)
 
     @cached_property
     def mantissa(self) -> float:
-        frac = self.raw_mantissa / (1 << 52)
+        frac = self.raw_mantissa / (1 << self.prec.mant_bits)
         if self.raw_exponent == 0:
             # zero and subnormals
             frac *= 2
@@ -113,10 +254,16 @@ class FloatInspector:
         return bool(self.raw_sign)
 
     def is_inf(self) -> bool:
-        return self.raw_exponent == _make_mask(self.EXP_BITS) and self.raw_mantissa == 0
+        return (
+            self.raw_exponent == _make_mask(self.prec.exp_bits)
+            and self.raw_mantissa == 0
+        )
 
     def is_nan(self) -> bool:
-        return self.raw_exponent == _make_mask(self.EXP_BITS) and self.raw_mantissa != 0
+        return (
+            self.raw_exponent == _make_mask(self.prec.exp_bits)
+            and self.raw_mantissa != 0
+        )
 
     def is_subnormal(self) -> bool:
         return self.raw_exponent == 0 and self.raw_mantissa != 0
--- tests/test_fp.py
+++ tests/test_fp.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import math
-import sys
 
 import pytest
 from hypothesis import assume, example, given, note
@@ -11,62 +10,109 @@ from hypothesis import strategies as st
 
 from yut23_utils.fp import (
     FloatInspector,
+    Precision,
     compare_ulp,
     float_to_int,
     int_to_float,
+    nextafter,
     ulp_diff,
 )
 
 
-@given(st.floats())
-@example(float("nan"))
-def test_float_to_int(x_flt: float) -> None:
-    x_int = float_to_int(x_flt)
+@st.composite
+def floats_with_prec(draw: st.DrawFn, *args, **kwargs) -> tuple[float, Precision]:
+    prec = draw(st.sampled_from(Precision))
+    if "width" in kwargs:
+        msg = "floats_with_prec() got an unexpected keyword argument 'width'"
+        raise TypeError(msg)
+    kwargs["width"] = prec.total_bits
+    x = draw(st.floats(*args, **kwargs))
+    return x, prec
+
+
+@st.composite
+def integers_with_prec(draw: st.DrawFn) -> tuple[int, Precision]:
+    prec = draw(st.sampled_from(Precision))
+    x = draw(st.integers(min_value=0, max_value=(1 << prec.total_bits) - 1))
+    return x, prec
+
+
+@given(floats_with_prec())
+@example((float("nan"), Precision.BINARY64))
+@example((float("nan"), Precision.BINARY32))
+@example((float("nan"), Precision.BINARY16))
+def test_float_to_int(entry: tuple[float, Precision]) -> None:
+    x_flt, prec = entry
+    x_int = float_to_int(x_flt, prec=prec)
     assert isinstance(x_int, int)
-    assert 0 <= x_int < 1 << 64
-    x_flt_2 = int_to_float(x_int)
+    assert 0 <= x_int < 1 << prec.total_bits
+    x_flt_2 = int_to_float(x_int, prec=prec)
     if math.isnan(x_flt):
         assert math.isnan(x_flt_2)
     else:
         assert x_flt_2 == x_flt
 
 
-@given(st.integers(min_value=0, max_value=(1 << 64) - 1))
-def test_int_to_float(x_int: int) -> None:
-    x_flt = int_to_float(x_int)
-    assert float_to_int(x_flt) == x_int
+@given(integers_with_prec())
+def test_int_to_float(entry: tuple[int, Precision]) -> None:
+    x_int, prec = entry
+    x_flt = int_to_float(x_int, prec=prec)
+    # binary16 NaNs aren't round-tripped correctly
+    assume(not (prec is Precision.BINARY16 and math.isnan(x_flt)))
+    assert float_to_int(x_flt, prec=prec) == x_int
+
+
+@given(st.floats(width=64), st.floats(width=64))
+def test_nextafter(x: float, y: float) -> None:
+    expected = math.nextafter(x, y)
+    actual = nextafter(x, y, prec=Precision.BINARY64)
+    # compare hex representations to handle NaN and signed zeros
+    assert actual.hex() == expected.hex()
 
 
 @st.composite
-def float_pairs(draw: st.DrawFn, max_ulps: int) -> tuple[float, float, int]:
-    a = draw(st.floats(allow_nan=False, allow_infinity=False))
+def float_pairs(draw: st.DrawFn, max_ulps: int) -> tuple[float, float, int, Precision]:
+    prec = draw(st.sampled_from(Precision))
+    a = draw(st.floats(allow_nan=False, allow_infinity=False, width=prec.total_bits))
     ulps = draw(st.integers(min_value=-max_ulps, max_value=max_ulps))
 
     b = a
     direction = math.copysign(math.inf, ulps)
     for _ in range(abs(ulps)):
-        b = math.nextafter(b, direction)
+        b = nextafter(b, direction, prec=prec)
         assume(not math.isinf(b))
 
-    note(f"a = {FloatInspector(a)}\nb = {FloatInspector(b)}")
-    return a, b, ulps
+    note(f"a = {FloatInspector(a, prec=prec)}\nb = {FloatInspector(b, prec=prec)}")
+    return a, b, ulps, prec
 
 
 @given(float_pairs(max_ulps=100))
-@example((0.0, -5e-324, -1))
-@example((5e-324, 0.0, -1))
-@example((-5e-324, 5e-324, 2))
-@example((1, 2, 1 << 52))
-@example((2, 4, 1 << 52))
-@example((0, 1, 0x3FF << 52))
-def test_ulp_diff(args: tuple[float, float, int]) -> None:
-    a, b, ulps = args
-    assert ulp_diff(a, b) == abs(ulps)
-    assert ulp_diff(b, a) == abs(ulps)  # pylint: disable=arguments-out-of-order
-    assert ulp_diff(a, b, include_sign=True) == ulps
-    assert ulp_diff(b, a, include_sign=True) == -ulps
-    assert ulp_diff(-b, -a, include_sign=True) == ulps
-    assert ulp_diff(-a, -b, include_sign=True) == -ulps
+@example((0.0, -5e-324, -1, Precision.BINARY64))
+@example((5e-324, 0.0, -1, Precision.BINARY64))
+@example((-5e-324, 5e-324, 2, Precision.BINARY64))
+@example((1, 2, 1 << 52, Precision.BINARY64))
+@example((2, 4, 1 << 52, Precision.BINARY64))
+@example((0, 1, 0x3FF << 52, Precision.BINARY64))
+@example((0.0, -1.4e-45, -1, Precision.BINARY32))
+@example((1.4e-45, 0.0, -1, Precision.BINARY32))
+@example((-1.4e-45, 1.4e-45, 2, Precision.BINARY32))
+@example((1, 2, 1 << 23, Precision.BINARY32))
+@example((2, 4, 1 << 23, Precision.BINARY32))
+@example((0, 1, 0x7F << 23, Precision.BINARY32))
+@example((0.0, -5.9e-8, -1, Precision.BINARY16))
+@example((5.9e-8, 0.0, -1, Precision.BINARY16))
+@example((-5.9e-8, 5.9e-8, 2, Precision.BINARY16))
+@example((1, 2, 1 << 10, Precision.BINARY16))
+@example((2, 4, 1 << 10, Precision.BINARY16))
+@example((0, 1, 0xF << 10, Precision.BINARY16))
+def test_ulp_diff(args: tuple[float, float, int, Precision]) -> None:
+    a, b, ulps, prec = args
+    assert ulp_diff(a, b, prec=prec) == abs(ulps)
+    assert ulp_diff(b, a, prec=prec) == abs(ulps)  # pylint: disable=arguments-out-of-order
+    assert ulp_diff(a, b, prec=prec, include_sign=True) == ulps
+    assert ulp_diff(b, a, prec=prec, include_sign=True) == -ulps
+    assert ulp_diff(-b, -a, prec=prec, include_sign=True) == ulps
+    assert ulp_diff(-a, -b, prec=prec, include_sign=True) == -ulps
 
 
 @pytest.mark.parametrize(
@@ -83,7 +129,7 @@ def test_ulp_diff_errors(a: float, b: float) -> None:
     with pytest.raises(ValueError, match="finite"):
         ulp_diff(a, b)
     with pytest.raises(ValueError, match="finite"):
-        ulp_diff(b, a)  # pylint: disable=arguments-out-of-order
+        ulp_diff(b, a)
     with pytest.raises(ValueError, match="finite"):
         ulp_diff(a, b, include_sign=True)
     with pytest.raises(ValueError, match="finite"):
@@ -91,13 +137,13 @@ def test_ulp_diff_errors(a: float, b: float) -> None:
 
 
 @given(float_pairs(max_ulps=100))
-@example((-5e-324, 5e-324, 2))
-def test_compare_ulp(args: tuple[float, float, int]) -> None:
-    a, b, ulps = args
-    assert compare_ulp(a, b, abs(ulps))
+@example((-5e-324, 5e-324, 2, Precision.BINARY64))
+def test_compare_ulp(args: tuple[float, float, int, Precision]) -> None:
+    a, b, ulps, prec = args
+    assert compare_ulp(a, b, abs(ulps), prec=prec)
     if abs(ulps) > 0:
-        assert not compare_ulp(a, b, abs(ulps) - 1)
-    assert compare_ulp(a, b, abs(ulps) + 1)
+        assert not compare_ulp(a, b, abs(ulps) - 1, prec=prec)
+    assert compare_ulp(a, b, abs(ulps) + 1, prec=prec)
 
 
 def test_compare_ulp_errors() -> None:
@@ -105,15 +151,18 @@ def test_compare_ulp_errors() -> None:
         compare_ulp(1.0, 1.0, ulps=-1)
 
 
-@given(st.floats())
-@example(float("nan"))
-def test_float_inspector(x: float) -> None:
-    fi = FloatInspector(x)
+@given(floats_with_prec())
+@example((float("nan"), Precision.BINARY64))
+@example((float("nan"), Precision.BINARY32))
+@example((float("nan"), Precision.BINARY16))
+def test_float_inspector(entry: tuple[float, Precision]) -> None:
+    x, prec = entry
+    fi = FloatInspector(x, prec=prec)
     note(f"fi={fi} (subnormal: {fi.is_subnormal()})")
     if not math.isnan(x):
         assert float(fi) == x
     # convert to an int to check raw NaN representation
-    assert float_to_int(float(fi)) == float_to_int(x)
+    assert float_to_int(float(fi), prec=prec) == float_to_int(x, prec=prec)
 
     # try manually reconstructing the value from the mantissa and exponent
     if math.isfinite(x):
@@ -126,5 +175,6 @@ def test_float_inspector(x: float) -> None:
     assert fi.is_negative() == is_negative
     assert fi.is_inf() == math.isinf(x)
     assert fi.is_nan() == math.isnan(x)
-    is_subnormal = 0 < abs(x) < sys.float_info.min
+    min_normal = 1.0 / (1 << (prec.bias - 1))
+    is_subnormal = 0 < abs(x) < min_normal
     assert fi.is_subnormal() == is_subnormal
